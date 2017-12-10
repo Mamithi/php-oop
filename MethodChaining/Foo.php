9dc9531b893403bcc91b0da73a0ff6953c3380f5
Mamithi <19lawrence93@gmail.com>
2017-12-10 09:47:21 +0300

    Understanding about method chaining

---
+++ MethodChaining/Foo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mamithi
+ * Date: 12/10/17
+ * Time: 9:43 AM
+ */
+
+class Foo
+{
+    public function bar()
+    {
+        echo 'foobar <br>';
+        return $this;
+    }
+
+    public function baz()
+    {
+        echo 'foobaz';
+    }
+}
---
+++ MethodChaining/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mamithi
+ * Date: 12/10/17
+ * Time: 9:43 AM
+ */
+
+require 'Foo.php';
+
+$foo = new Foo();
+
+$foo->bar()->baz();
