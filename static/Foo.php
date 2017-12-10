b6465fcda9e8e1c4a1f47777dd46fc7e4b39294a
Mamithi <19lawrence93@gmail.com>
2017-12-10 09:41:57 +0300

    Learning about static methods

---
+++ static/Foo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mamithi
+ * Date: 12/10/17
+ * Time: 9:35 AM
+ */
+
+class Foo
+{
+    public static $bar = 'foobar';
+
+    public static function hello($var){
+        return $var;
+    }
+}
---
+++ static/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mamithi
+ * Date: 12/10/17
+ * Time: 9:35 AM
+ */
+
+require 'Foo.php';
+
+echo Foo::$bar;
